b4f85c01f35646c2c6fffdc718642325018a3937
Mohammed Alshiekh <sahabi@gmail.com>
2018-02-25 22:43:52 -0600

    completed the training and prediction for linear basis functions regression

--- src/ML/Models.hs
+++ src/ML/Models.hs
@@ -1,41 +1,73 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Models where
 
-import qualified Data.Vector as V
-import qualified Data.Vector.Generic as G
+import Foreign.Storable (Storable)
+import Numeric.LinearAlgebra ((<.>), (<>), (#>), pinv, sumElements, Container)
+import Numeric.LinearAlgebra.Data (vector, matrix, Matrix, (><), Vector, tr'
+                                  , toList, fromList)
 
--- ==================================================
-
-class Trainable m where
-  train :: m a b -> a -> m a b
+class Trainable m a b where
+  train :: m a b -> [(a,b)] -> m a b
 
 class Predictive m where
   predict :: m a b -> a -> b
 
+vmap f = fromList . fmap f . toList
 
--- ===================================================
-type LBInput = V.Vector Float
+type LBInput a = [a]
+type LabeledSet a = [(LBInput a, Double)]
+type LBWeights = Vector Double
+type LBFuncs a = [a -> Double]
 
-type LBWeights = V.Vector Float
-
-type LBFuncs a = V.Vector (a -> Float)
-
-data LBReg a b = LBReg { lbFuncs :: LBFuncs LBInput
+data LBReg a b = LBReg { lbFuncs :: LBFuncs (LBInput Double)
                        , lbWeights :: LBWeights
                        , lbPredict :: LBWeights
-                                   -> LBFuncs LBInput
+                                   -> LBFuncs (LBInput Double)
                                    -> a
                                    -> b
+                       , lbError :: Double
                        }
 
-instance Trainable LBReg where
-  train m d = m
+instance Trainable LBReg [Double] Double where
+  train = trainLBReg
 
 instance Predictive LBReg where
   predict m = lbPredict m (lbWeights m) (lbFuncs m)
 
-mkLBReg :: LBFuncs LBInput -> LBReg Float Float
-mkLBReg bfs = LBReg { lbFuncs = bfs
-                    , lbWeights = V.fromList $ replicate (V.length bfs) 0
-                    , lbPredict = \x y w -> 1 }
+updateWeights x w e = x { lbWeights = w, lbError = e }
 
--- ==================================================
+trainLBReg :: LBReg [Double] Double -> [([Double], Double)] -> LBReg [Double] Double
+trainLBReg m d = updateWeights m w e where
+  t = vector (fmap snd d)
+  fs = lbFuncs m
+  dps = fmap fst d
+  p = lbPredict m
+  w = pinv dm #> t where
+    dm = (length dps >< length fs) $ fs <*> dps
+  e = sosError t pr where pr = vector $ fmap (p w fs) dps
+
+sosError :: (Num (Vector a), Floating a, Container Vector a) => Vector a -> Vector a -> a
+sosError t pr = sumElements $ vmap ((/2) . (**2)) $ t - pr
+
+lbEval :: LBWeights -> LBFuncs (LBInput Double) -> LBInput Double -> Double
+lbEval w fs d = w <.> vector (zipWith ($) fs $ replicate (length fs) d)
+
+mkLBId :: Int -> LBFuncs (LBInput Double)
+mkLBId n = const 1 : [ (!!x) | x <- [0 .. n-1]]
+
+mkLBReg :: LBFuncs (LBInput Double) -> LabeledSet Double -> LBReg (LBInput Double) Double
+mkLBReg bfs = train LBReg { lbFuncs = bfs
+                          , lbWeights = vector $ replicate (length bfs + 1) 0
+                          , lbPredict = lbEval
+                          , lbError = 999
+                          }
+
+mkSLBReg :: LabeledSet Double -> LBReg (LBInput Double) Double
+mkSLBReg  ds = train LBReg { lbFuncs = mkLBId n
+                           , lbWeights = vector $ replicate n 0
+                           , lbPredict = lbEval
+                           , lbError = 999
+                           } ds where n = length $ fst $ head ds
